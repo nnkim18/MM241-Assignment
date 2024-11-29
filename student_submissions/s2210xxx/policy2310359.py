9310f680fb590cc35f766fed9425202727f46e46
Chung Dinh Hoang <hgchung.dinh@gmail.com>
2024-11-29 10:35:41 +0700

    Fixing "def" to "class"

--- student_submissions/s2210xxx/policy2310359.py
+++ student_submissions/s2210xxx/policy2310359.py
@@ -1,13 +1,34 @@
 from policy import Policy
 
 
-def Policy2310359(Policy):
+class Policy2310359(Policy):
     def __init__(self):
         # Student code here
         pass
 
     def get_action(self, observation, info):
         # Student code here
+        list_prods = observation["products"]
+
+        prod_size = [0, 0]
+        stock_idx = -1
+        pos_x, pos_y = 0, 0
+
+        for prod in list_prods:
+            if (prod["quantity"] > 0):
+                prod_size = prod["size"]
+
+                for i, stock in enumerate(observation["stocks"]):
+                    stock_w, stock_h = self._get_stock_size_(stock)
+                    prod_w, prod_h = prod_size
+
+                    if stock_w < prod_h or stock_h < prod_h:
+                        continue
+
+                    if not self._can_place_(stock, (0, 0), prod_size):
+                        continue
+
+                    return {"stock_idx": i, "size": prod_size, "position": (0, 0)}
         pass
 
     # Student code here
