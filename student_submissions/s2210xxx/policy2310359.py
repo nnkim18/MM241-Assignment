f2bf24664311054319251cab91315f08482b771d
Chung Dinh Hoang <hgchung.dinh@gmail.com>
2024-11-30 23:13:13 +0700

    Add Skyline's calculate_local_waste
    It refactors some other code.

--- student_submissions/s2210xxx/policy2310359.py
+++ student_submissions/s2210xxx/policy2310359.py
@@ -14,8 +14,11 @@ class Policy2310359(Policy):
             self.init_skyline_set(observation["stocks"])
             print(self.skylines)
 
-        ### this makes code run normal
+        for prod in observation["products"]:
+            print("Waste:", [self.calculate_minimum_local_waste(prod, observation["stocks"][0], 0)])
+            break
 
+        ### this naive algorithm just makes code run normal
         list_prods = observation["products"]
 
         prod_size = [0, 0]
@@ -49,7 +52,7 @@ class Policy2310359(Policy):
             self.height = height
 
         def _intersect_(self, prod_size, prod_pos) -> bool:
-            # intersect here means positive common areas, which makes this invalid
+            # intersect here means positive common areas, which makes this placement invalid
 
             prod_x_start, prod_y = prod_pos
             prod_w, prod_h = prod_size
@@ -60,7 +63,15 @@ class Policy2310359(Policy):
             if (prod_y >= self.height): return False
             
             return True
+        
+        def _calculate_local_waste_(self, prod_size, prod_pos) -> int:
+            prod_x_start, prod_y = prod_pos
+            prod_w, prod_h = prod_size
+            prod_x_end = prod_x_start + prod_w
 
+            if (prod_x_end <= self.x_start or prod_x_start >= self.x_end): return 0
+
+            return (prod_y - self.height) * (min(prod_x_end, self.x_end) - max(prod_x_start, self.x_start))
 
     #####################
 
@@ -69,9 +80,9 @@ class Policy2310359(Policy):
     def init_skyline_set(self, stocks):
         # this will initialize the skyline set
         for i, stock in enumerate(stocks):
-            self.skylines += [[Policy2310359.SkylinePart(0, self._get_stock_size_(stock), 0)]]
+            self.skylines += [[Policy2310359.SkylinePart(0, self._get_stock_size_(stock)[0], 0)]]
 
-    def local_waste_calc(self, product, stock, stock_idx):
+    def calculate_minimum_local_waste(self, product, stock, stock_idx):
         # Current idea: 
         # Each time we add the product to make smallest local waste
         # This function calculates local waste
@@ -79,7 +90,8 @@ class Policy2310359(Policy):
         prod_w, prod_h = product["size"]
 
         WONT_USE = 1e18
-        current_waste = WONT_USE
+        waste_area = WONT_USE
+        placement_choice = (-1, -1)
 
         # First, we try placing on the leftmost of each part of skyline
         for part_idx, part in enumerate(self.skylines[stock_idx]):
@@ -92,11 +104,18 @@ class Policy2310359(Policy):
             # Must not intersect other part
             ok = True
             for other_part in self.skylines[part_idx]:
-                if other_part.insersect(product["size"], (pos_x, pos_y)):
+                if other_part._intersect_(product["size"], (pos_x, pos_y)):
                     ok = False
                     break
             
             if not ok: continue
 
-            waste_area = 0
+            current_waste_area = 0
+            for each_part in self.skylines[stock_idx]:
+                current_waste_area += each_part._calculate_local_waste_((prod_w, prod_h), (pos_x, pos_y))
 
+            if waste_area > current_waste_area:
+                waste_area = current_waste_area
+                placement_choice = (pos_x, pos_y)
+        
+        return int(waste_area)
