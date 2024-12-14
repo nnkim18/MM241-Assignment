491f7ddcaa466bd1204f8011a9c676b1d0fa4323
Lena-user <le.lena5415@gmail.com>
2024-12-15 03:44:33 +0700

    Rename policy to Policy2211581_2211538_2211556_2213039 and move in to folder s2211581_2211538_2211556_2213039

--- student_submissions/s2211581_2211538_2211556_2213039/policy2211581_2211538_2211556_2213039.py
+++ student_submissions/s2211581_2211538_2211556_2213039/policy2211581_2211538_2211556_2213039.py
@@ -127,7 +127,7 @@ class DynamicPriorityPolicyKnapsack(Policy):
 
         return placement_options
 
-class Policy2211581(Policy):
+class Policy2211581_2211538_2211556_2213039(Policy):
     def __init__(self, policy_id=1):
         assert policy_id in [1, 2], "Policy ID must be 1 or 2"
 
