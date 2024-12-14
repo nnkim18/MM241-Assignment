62fa92fbf022f3e34e6604e869f5832324af1da2
nangsontay <143306990+nangsontay@users.noreply.github.com>
2024-12-15 00:04:35 +0700

    Update policy2210xxx.py

--- student_submissions/s2210xxx/policy2210xxx.py
+++ student_submissions/s2210xxx/policy2210xxx.py
@@ -1,19 +1,27 @@
+# from student_submissions.s2210xxx.policy_genetic import policy_genetic
 from policy import Policy
+from student_submissions.s2210xxx.floorceil import FloorCeil
+from student_submissions.s2210xxx.genetic import GeneticPolicy
 
 
 class Policy2210xxx(Policy):
-    def __init__(self, policy_id=1):
+    def __init__(self, policy_id):
+        super().__init__()  # Call parent class constructor
         assert policy_id in [1, 2], "Policy ID must be 1 or 2"
 
-        # Student code here
+        # Initialize policy attribute
+        self.policy = None
+
         if policy_id == 1:
-            pass
+            self.policy = FloorCeil()  # Create instance of BFDH policy
         elif policy_id == 2:
-            pass
+            # Initialize policy 2 here
+            self.policy = GeneticPolicy()
 
     def get_action(self, observation, info):
-        # Student code here
-        pass
+        if self.policy is not None:
+            return self.policy.get_action(observation, info)
+        return None  # Or implement default behavior
 
     # Student code here
     # You can add more functions if needed
