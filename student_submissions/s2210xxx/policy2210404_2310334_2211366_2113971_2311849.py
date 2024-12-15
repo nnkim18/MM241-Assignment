0bb371fb9b9047f85c38f9f58303a31aa5d6e606
congduythanhdao <cong.daoduythanh09@hcmut.edu.vn>
2024-12-15 23:49:42 +0700

    code changes

--- student_submissions/s2210xxx/policy2210404_2310334_2211366_2113971_2311849.py
+++ student_submissions/s2210xxx/policy2210404_2310334_2211366_2113971_2311849.py
@@ -3,7 +3,7 @@ import random
 import numpy as np
 
 
-class Policy2210xxx(Policy):
+class Policy2210404_2310334_2211366_2113971_2311849(Policy):
     def __init__(self, policy_id=1):
         assert policy_id in [1, 2], "Policy ID must be 1 or 2"
         self.policy_id = policy_id
