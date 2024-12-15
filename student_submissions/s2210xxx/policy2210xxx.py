2ecf51e33fb8bcf811e971a2dec6fe7a59ef14a1
HacvaVang <phuthinh1104@gmail.com>
2024-12-15 23:15:46 +0700

    Add files via upload

--- student_submissions/s2210xxx/policy2210xxx.py
+++ student_submissions/s2210xxx/policy2210xxx.py
@@ -1,19 +1,9 @@
 from policy import Policy
-
+from student_submissions.s2313293_2313950_2310280.policy2313293_2313950_2310280 import Policy2313293_2313950_2310280
 
 class Policy2210xxx(Policy):
-    def __init__(self, policy_id=1):
-        assert policy_id in [1, 2], "Policy ID must be 1 or 2"
-
-        # Student code here
-        if policy_id == 1:
-            pass
-        elif policy_id == 2:
-            pass
+    def __init__(self, policy_id = 1):
+        self.policy = Policy2313293_2313950_2310280(policy_id)
 
     def get_action(self, observation, info):
-        # Student code here
-        pass
-
-    # Student code here
-    # You can add more functions if needed
+        return self.policy.get_action(observation, info)
