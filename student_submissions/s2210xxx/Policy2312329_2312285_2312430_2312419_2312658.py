0e9879cbe811aede0dc3dc319faf4a11f30bee7a
nguyencldv <nguyen.buitrong09@hcmut.edu.vn>
2024-12-15 18:20:01 +0700

    Update Policy2312329_2312285_2312430_2312419_2312658.py
    Final version

--- student_submissions/s2210xxx/Policy2312329_2312285_2312430_2312419_2312658.py
+++ student_submissions/s2210xxx/Policy2312329_2312285_2312430_2312419_2312658.py
@@ -13,7 +13,9 @@ class Policy2312329_2312285_2312430_2312419_2312658(Policy):
             self.list_prods = []
             pass
         elif self.policy_id == 2:
-            self.square_stock = np.zeros(100)
+            self.T = 00
+            self.square_stock = np.zeros(100100)
+            self.obs = 0
             pass
 
     def get_action(self, observation, info):
@@ -96,145 +98,138 @@ class Policy2312329_2312285_2312430_2312419_2312658(Policy):
         return None, None
     
 
-    def _findmaxmin_square_(self, observation):
-        stocks = observation["stocks"]
-        max_stock = 0
-        min_stock = float("inf")
-        for stock in stocks:
-            stock_w, stock_h = self._get_stock_size_(stock)
-            area = stock_w * stock_h
-            max_stock = max(max_stock, area)
-            min_stock = min(min_stock, area)
-        return max_stock, min_stock
+    def SA_help(self, stock, square_stock, i, k):
+        #stock_w2, stock_h2 = self._get_stock_size_(stock2)
+        DE = self.square(stock, i, square_stock) - self.square(stock, k, square_stock)
+        return DE
+        #pass
 
-    def _avg_square_(self, observation):
-        max_stock, min_stock = self._findmaxmin_square_(observation)
-        return (max_stock + min_stock) / 2
+    def set_obs(self, observation):
+        self.obs = observation
+        pass
 
-    def _avg_prod_(self, observation):
-        list_prods = observation["products"]
-        max_p = 0
-        min_p = float("inf")
-        for prod in list_prods:
-            prod_w, prod_h = prod["size"]
-            area = prod_w * prod_h
-            max_p = max(max_p, area)
-            min_p = min(min_p, area)
-        return (max_p + min_p) / 2
+    def square(self, stock, i, square_stock):
+        stock_w, stock_h = self._get_stock_size_(stock[i])
+        S_remain = stock_w * stock_h - square_stock[i]
+        return S_remain
+        #pass
 
-    def _sort_products_by_size_(self, observation):
-        """Sắp xếp sản phẩm theo thứ tự: trung bình -> lớn -> nhỏ."""
-        products = observation["products"]
-        areas = [prod["size"][0] * prod["size"][1] for prod in products]
-        average_area = sum(areas) / len(areas)
-
-        medium_products = [
-            prod
-            for prod in products
-            if abs(prod["size"][0] * prod["size"][1] - average_area) <= average_area * 0.3
-        ]
-        large_products = [
-            prod for prod in products if prod["size"][0] * prod["size"][1] > average_area
-        ]
-        small_products = [
-            prod for prod in products if prod["size"][0] * prod["size"][1] < average_area
-        ]
-
-        medium_products = sorted(medium_products, key=lambda p: p["size"][0] * p["size"][1])
-        large_products = sorted(
-            large_products, key=lambda p: p["size"][0] * p["size"][1], reverse=True
-        )
-        small_products = sorted(small_products, key=lambda p: p["size"][0] * p["size"][1])
-
-        return medium_products + large_products + small_products
-    
-    def _find_position_(self, stock, prod_size):
-        """Tìm vị trí đặt sản phẩm trong stock."""
-        stock_w, stock_h = self._get_stock_size_(stock)
-        prod_w, prod_h = prod_size
-        for x in range(stock_w - prod_w + 1):
-            for y in range(stock_h - prod_h, -1, -1):
-                if self._can_place_(stock, (x, y), prod_size):
-                    return x, y, prod_size
-        prod_size[0], prod_size[1] = prod_size[1], prod_size[0]
-        for x in range(stock_w - prod_size[0] + 1):
-            for y in range(stock_h - prod_size[1], -1, -1):
-                if self._can_place_(stock, (x, y), prod_size):
-                    return x, y, prod_size
-        prod_size[0], prod_size[1] = prod_size[1], prod_size[0]
-        return None, None, prod_size
+    def set_T(self, observation):
+        list = observation["products"]
+        for prod in list:
+            self.T += prod["quantity"]
+        pass                  
 
     def get_action2(self, observation, info):
-        list_prods = observation["products"]
-        list_prods = sorted(list_prods, key=lambda x: x["size"][0] * x["size"][1], reverse=True)
-        #list_prods = self._sort_products_by_size_(observation)
+     self.set_obs(observation)
+     square_stock = self.square_stock
+     self.set_T(observation)
+  
+     list_prods = observation["products"]  # Danh sách sản phẩm
+     list_prods = sorted(list_prods, key=lambda x: x["size"][0] * x["size"][1], reverse=True)
+     stocks = observation["stocks"]
+     stock_idx, pos_x, pos_y = -1, None, None
+     prod_size = [0, 0]
 
-        stocks = observation["stocks"]
-        stock_idx, pos_x, pos_y = -1, None, None
-        prod_size = [0, 0]
 
-        for prod in list_prods:
-            prod_size = prod["size"]
-            prod_w, prod_h = prod_size
-            if prod["quantity"] > 0:
-                if 0.5 < prod_w / prod_h < 1.5 and 0.8 < prod_w * prod_h / self._avg_prod_(observation) < 1.5:
-                    for i, stock in enumerate(stocks):
-                        stock_w, stock_h = self._get_stock_size_(stock)
-                        for k in range(4, 0, -1):
-                            for h in range(4, 0, -1):
-                                if 1 < stock_h / (k * prod_h) < 1.3 and 1 < stock_w / (h * prod_w) < 1.3 :
-                                    if self. square_stock[i] >= 0:
-                                        pos_x, pos_y, prod_size = self._find_position_(stock, prod_size)
-                                        if pos_x is not None and pos_y is not None:
-                                            stock_idx = i
-                                            break
-                                    elif self.square_stock[i] == 0:
-                                        if (prod["quantity"] > k * h ):
-                                        #if (self.square_stock[i] != 0 or (self.square_stock[i] == 0 and prod["quantity"] > k * h - prod["quantity"])):
-                                            pos_x, pos_y, prod_size = self._find_position_(stock, prod_size)
-                                            if pos_x is not None and pos_y is not None:
-                                                stock_idx = i
-                                                break
-                                    elif (prod["quantity"] < k * h ):
-                                        continue
-                                        
-                            if pos_x is not None and pos_y is not None:
+     for prod in list_prods:
+        if prod["quantity"] > 0:
+            prod_size = prod["size"][:]
+
+            for i, stock in enumerate(stocks):
+                stock_w, stock_h = self._get_stock_size_(stock)
+
+                for rotated in [False, True]:  
+                    if rotated:
+                        prod_size[0], prod_size[1] = prod_size[1], prod_size[0]
+                    prod_w, prod_h = prod_size
+
+                    if stock_w >= prod_w and stock_h >= prod_h :
+                        if prod_h >= prod_w:
+                         for x in range(stock_w - prod_w + 1):
+                            
+                            for y in range(stock_h - prod_h + 1):
+                                
+                                if self._can_place_(stock, (x, y), prod_size):
+                                    pos_x, pos_y = x, y
+                                    stock_idx = i
+                             
+                            if pos_x is not None:  
                                 break
-                        if pos_x is not None and pos_y is not None:
-                            break
+                        if prod_h < prod_w:
+                          for y in range(stock_h - prod_h + 1):
+                            
+                            for x in range(stock_w - prod_w + 1):
+                                
+                                if self._can_place_(stock, (x, y), prod_size):
+                                    pos_x, pos_y = x, y
+                                    stock_idx = i
+ 
+                            if pos_x is not None:  
+                                break
+                    
                     if pos_x is not None:
-                        break
-                    if pos_x is None:
-                        for i, stock in enumerate(stocks):
-                            stock_w, stock_h = self._get_stock_size_(stock)
-                            if (stock_w > 1.2 * stock_h and prod_size[0] > 1.2 * prod_size[1]):
-                                prod_size[0], prod_size[1] = prod_size[1], prod_size[0]
-                            elif (stock_h > 1.2 * stock_w and prod_size[1] > 1.2 * prod_size[1]):
-                                prod_size[0], prod_size[1] = prod_size[1], prod_size[0]
-
-                            pos_x, pos_y, prod_size = self._find_position_(stock, prod_size)
-                            if pos_x is not None and pos_y is not None:
-                                stock_idx = i
+                       for k in range(i + 1, min (i + 4, len(stocks))):
+                        
+                        stock2 = stocks[k]
+                        
+                        stock_w2, stock_h2 = self._get_stock_size_(stock2)
+                        if stock_w2 < prod_w or stock_h2 < prod_h:
+                            continue
+                        p = self.square(stocks, k, square_stock)/ (prod_h * prod_w)
+                        DE = self.SA_help(stocks, square_stock, i , k)
+                        
+                        if prod_h >= prod_w:
+                         for x1 in range(stock_w2 - prod_w + 1):
+                            for y1 in range(stock_h2 - prod_h + 1):
+                                if self._can_place_(stock2, (x1, y1), prod_size):
+                                    if DE < 0 and -(DE)  > 2 * prod_h * prod_w :
+                                        pos_x, pos_y = x1, y1
+                                        stock_idx = k
+                                        
+                                        break
+                                    if DE > 0 and np.exp(-DE/self.T) > 0.8 and p < 5:
+                                        pos_x, pos_y = x1, y1
+                                        stock_idx = k
+                                        
+                                        break
+                                    break # xem xet co break hay ko
+                            if stock_idx == k:
                                 break
-
-                if pos_x is None:
-                    for i, stock in enumerate(stocks):
-                        stock_w, stock_h = self._get_stock_size_(stock)
-                        if (stock_w > 1.2 * stock_h and prod_size[0] > 1.2 * prod_size[1]):
-                            prod_size[0], prod_size[1] = prod_size[1], prod_size[0]
-                        elif (stock_h > 1.2 * stock_w and prod_size[1] > 1.2 * prod_size[0]):
-                            prod_size[0], prod_size[1] = prod_size[1], prod_size[0]
-
-                        pos_x, pos_y, prod_size = self._find_position_(stock, prod_size)
-                        if pos_x is not None and pos_y is not None:
-                            stock_idx = i
+                        if prod_h < prod_w:
+                         for y1 in range(stock_h2 - prod_h + 1):
+                            for x1 in range(stock_w2 - prod_w + 1):
+                                if self._can_place_(stock2, (x1, y1), prod_size):
+                                    if DE < 0 and -(DE) > 2 * prod_h * prod_w :
+                                        pos_x, pos_y = x1, y1
+                                        stock_idx = k
+                                        
+                                        break
+                                    if DE > 0 and np.exp(-DE/self.T) > 0.2 and p < 5:
+                                        pos_x, pos_y = x1, y1
+                                        stock_idx = k
+                                        break
+                                    break 
+                            if stock_idx == k:
+                                break
+                        if stock_idx == k:
+                            #m = 0
                             break
+                       if stock_idx == k:
+                          break
+                    if pos_x is not None:  # Thoát khỏi vòng lặp xoay
+                        break
+                if pos_x is not None:  # Thoát khỏi vòng lặp stock
+                    break
+        self.T = 0.7 * self.T
+        if self.T < 0:
+            self.T = 1   
+        if pos_x is not None:  
+            break
+     self.square_stock = square_stock
 
-            if pos_x is not None and pos_y is not None:
-                self.square_stock[stock_idx] += prod_size[0] * prod_size[1]
-                break
+     return {"stock_idx": stock_idx, "size": prod_size, "position": (pos_x, pos_y)}
 
-        return {"stock_idx": stock_idx, "size": prod_size, "position": (pos_x, pos_y)}
 
     # Student code here
     # You can add more functions if needed
