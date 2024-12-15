5141de6f9642b5019c6c8a4bf5a15c6eff407c5a
vinhnguyencong2005 <vinh.nguyencong@hcmut.edu.vn>
2024-12-15 10:33:49 +0700

    2353133 2353329 2353330 2352651 2352241 - Group 19

--- student_submissions/s2210xxx/policy2210xxx.py
+++ student_submissions/s2210xxx/policy2210xxx.py
@@ -1,5 +1,5 @@
 from policy import Policy
-
+from student_submissions.s2210xxx.policy2353329 import Policy2353329_1, Policy2353329_2
 
 class Policy2210xxx(Policy):
     def __init__(self, policy_id=1):
@@ -7,13 +7,13 @@ class Policy2210xxx(Policy):
 
         # Student code here
         if policy_id == 1:
-            pass
+            self.policy = Policy2353329_1()
         elif policy_id == 2:
-            pass
+            self.policy = Policy2353329_2()
 
     def get_action(self, observation, info):
         # Student code here
-        pass
+        return self.policy.get_action(observation, info)
 
     # Student code here
     # You can add more functions if needed
