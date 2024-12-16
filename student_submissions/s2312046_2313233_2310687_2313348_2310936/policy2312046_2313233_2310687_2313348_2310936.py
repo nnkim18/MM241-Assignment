ba3bfdea190f7980a1956f938750ea34a93da1a0
NgocMinhUniversityProjects <minh.buingocbkhoa@hcmut.edu.vn>
2024-12-16 19:57:38 +0700

    Update policy2312046_2313233_2310687_2313348_2310936.py

--- student_submissions/s2312046_2313233_2310687_2313348_2310936/policy2312046_2313233_2310687_2313348_2310936.py
+++ student_submissions/s2312046_2313233_2310687_2313348_2310936/policy2312046_2313233_2310687_2313348_2310936.py
@@ -3,7 +3,7 @@ from s2312046_2313233_2310687_2313348_2310936.ppo_policy import PPO
 from s2312046_2313233_2310687_2313348_2310936.heuristic_policy import CPolicy
 
 
-class policy2312046_2313233_2310687_2313348_2310936(Policy):
+class Policy2312046_2313233_2310687_2313348_2310936(Policy):
     def __init__(self, policy_id=1):
         assert policy_id in [1, 2], "Policy ID must be 1 or 2"
         self.policy = None
