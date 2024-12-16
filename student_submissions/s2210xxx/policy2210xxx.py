4486347e43e6718ea59eeaebd3c62f7accfe7d2c
HacvaVang <phuthinh1104@gmail.com>
2024-12-17 00:27:45 +0700

    Update policy2210xxx.py

--- student_submissions/s2210xxx/policy2210xxx.py
+++ student_submissions/s2210xxx/policy2210xxx.py
@@ -1,9 +1,19 @@
 from policy import Policy
-from student_submissions.s2313293_2313950_2310280.policy2313293_2313950_2310280 import Policy2313293_2313950_2310280
+
 
 class Policy2210xxx(Policy):
-    def __init__(self, policy_id = 1):
-        self.policy = Policy2313293_2313950_2310280(policy_id)
+    def __init__(self, policy_id=1):
+        assert policy_id in [1, 2], "Policy ID must be 1 or 2"
+
+        # Student code here
+        if policy_id == 1:
+            pass
+        elif policy_id == 2:
+            pass
 
     def get_action(self, observation, info):
-        return self.policy.get_action(observation, info)
+        # Student code here
+        pass
+
+    # Student code here
+    # You can add more functions if needed
