b577bb13bfb03f1f975c40a1d866a034d120335a
eenosse <86368392+eenosse@users.noreply.github.com>
2024-12-16 20:41:38 +0700

    solve conflict

--- student_submissions/s2210xxx/policy2210xxx.py
+++ student_submissions/s2210xxx/policy2210xxx.py
@@ -14,3 +14,6 @@ class Policy2210xxx(Policy):
     def get_action(self, observation, info):
         # Student code here
         pass
+
+    # Student code here
+    # You can add more functions if needed
