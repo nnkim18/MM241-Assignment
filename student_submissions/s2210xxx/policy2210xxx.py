6b10d0e64b68eb59b422bccacd337a1ffe602257
Windyy-1007 <namphongtip02@gmail.com>
2024-12-16 20:36:22 +0700

    Update policy2210xxx.py

--- student_submissions/s2210xxx/policy2210xxx.py
+++ student_submissions/s2210xxx/policy2210xxx.py
@@ -1,10 +1,15 @@
 from policy import Policy
 
 
-def Policy2210xxx(Policy):
-    def __init__(self):
+class Policy2210xxx(Policy):
+    def __init__(self, policy_id=1):
+        assert policy_id in [1, 2], "Policy ID must be 1 or 2"
+
         # Student code here
-        pass
+        if policy_id == 1:
+            pass
+        elif policy_id == 2:
+            pass
 
     def get_action(self, observation, info):
         # Student code here
