e2c107f74b4bdc8cbea1a3adfa46f9cefaf3f9d5
Windyy-1007 <namphongtip02@gmail.com>
2024-12-16 20:23:03 +0700

    resubmit policy.py

---
+++ student_submissions/s2210xxx/policy2210xxx.py
@@ -0,0 +1,14 @@
+from policy import Policy
+import heapq
+import numpy as np  # Add this import
+import pulp
+
+class Policy2352906(Policy):
+    def __init__(self):
+        pass
+    
+    def get_action(self, observation, info):
+        pass
+
+
+
