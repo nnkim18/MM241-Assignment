0066424a6d41883fb3a4af4e6120872878ddd387
Nguyễn Đặng Thế Hoàng <hoang9a2lqd@gmail.com>
2024-12-16 20:51:30 +0700

    Fix

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import gym_cutting_stock
 import gymnasium as gym
 from policy import GreedyPolicy, RandomPolicy
-from student_submissions.s2311051_2352890_2310467_2314019_2312054.policy2311051_2352890_2310467_2314019_2312054 import Policy2311051_2352890_2310467_2314019_2312054
+from student_submissions.s2210xxx.policy2210xxx import Policy2210xxx
 
 # Create the environment
 env = gym.make(
     "gym_cutting_stock/CuttingStock-v0",
     render_mode="human",  # Comment this line to disable rendering
 )
-NUM_EPISODES = 2
+NUM_EPISODES = 100
 
 if __name__ == "__main__":
     # Reset the environment
     observation, info = env.reset(seed=42)
 
     # Test GreedyPolicy
-    gd_policy = Policy2311051_2352890_2310467_2314019_2312054(policy_id=1)
+    gd_policy = GreedyPolicy()
     ep = 0
     while ep < NUM_EPISODES:
         action = gd_policy.get_action(observation, info)
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     observation, info = env.reset(seed=42)
 
     # Test RandomPolicy
-    rd_policy = Policy2311051_2352890_2310467_2314019_2312054(policy_id=2)
+    rd_policy = RandomPolicy()
     ep = 0
     while ep < NUM_EPISODES:
         action = rd_policy.get_action(observation, info)
