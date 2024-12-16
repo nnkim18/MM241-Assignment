587f61719e2a693af1be4c2d3f845cdc71b47647
NgocMinhUniversityProjects <minh.buingocbkhoa@hcmut.edu.vn>
2024-12-16 20:24:37 +0700

    Update policy2312046_2313233_2310687_2313348_2310936.py
    Change import path

--- student_submissions/s2312046_2313233_2310687_2313348_2310936/policy2312046_2313233_2310687_2313348_2310936.py
+++ student_submissions/s2312046_2313233_2310687_2313348_2310936/policy2312046_2313233_2310687_2313348_2310936.py
@@ -1,6 +1,6 @@
 from policy import Policy
-from s2312046_2313233_2310687_2313348_2310936.ppo_policy import PPO
-from s2312046_2313233_2310687_2313348_2310936.heuristic_policy import CPolicy
+from .ppo_policy import PPO
+from .heuristic_policy import CPolicy
 
 
 class Policy2312046_2313233_2310687_2313348_2310936(Policy):
