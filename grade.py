48152a5defd898a80d11840db77ea77ac58a0fa5
Martin Nguyen <nguyenquangduc2000@gmail.com>
2024-12-30 03:48:18 -0800

    Add grading code

---
+++ grade.py
@@ -0,0 +1,106 @@
+import argparse
+import importlib
+import json
+
+import os
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, TimeoutError
+
+import gym_cutting_stock
+import gymnasium as gym
+
+from configs import CONFIGS
+from tqdm import tqdm
+
+
+def import_submodule(full_name):
+    module_name = full_name.split(".")
+    submodule_name = module_name[-1]
+    module_name = ".".join(module_name[:-1])
+    module = importlib.import_module(module_name)
+    submodule = getattr(module, submodule_name)
+    return submodule
+
+
+def create_env(config):
+    env = gym.make(
+        "gym_cutting_stock/CuttingStock-v0",
+        **config,
+    )
+    return env
+
+
+def run_one_episode(config, policy):
+    env = create_env(config)
+    observation, info = env.reset(seed=config["seed"])
+
+    terminated = False
+    truncated = False
+    info = None
+
+    while not terminated and not truncated:
+        action = policy.get_action(observation, info)
+        observation, reward, terminated, truncated, info = env.step(action)
+
+    return info
+
+
+def grade_one_group(grroup_folder):
+    module_name = "policy" + grroup_folder[1:]
+    module_name = (
+        f"student_submissions.{grroup_folder}.{module_name}.Policy{grroup_folder[1:]}"
+    )
+    policy_class = import_submodule(module_name)
+
+    policy = policy_class()
+
+    results = []
+    for config in CONFIGS:
+        executor = ThreadPoolExecutor(max_workers=1)
+        # Pass the arguments to the function via `submit`
+        future = executor.submit(run_one_episode, config, policy)
+
+        try:
+            result = future.result(timeout=300)  # Timeout after 5 seconds
+        except TimeoutError:
+            print("Function execution exceeded the time limit!")
+            result = {"filled_ratio": 1.0, "trim_loss": 1.0}
+
+        results.append(result)
+
+    # Save the results
+    with open(f"student_submissions/{grroup_folder}/grade.json", "w") as f:
+        json.dump(results, f, indent=4)
+
+    return results
+
+
+def grade_all_groups(args):
+    group_folders = os.listdir("student_submissions")
+    group_folders.remove("s2210xxx")
+
+    # Sort the group folders
+    group_folders = sorted(group_folders)
+
+    # Ensure that the group_folders are folders
+    group_folders = [
+        group_folder
+        for group_folder in group_folders
+        if os.path.isdir(f"student_submissions/{group_folder}")
+        and group_folder.startswith("s")
+    ]
+
+    with ProcessPoolExecutor(max_workers=args.num_workers) as executor:
+        results = list(
+            tqdm(executor.map(grade_one_group, group_folders), total=len(group_folders))
+        )
+
+    if len(results) == len(group_folders):
+        print("Grading completed successfully!")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num_workers", type=int, default=4)
+    args = parser.parse_args()
+
+    grade_all_groups(args)
